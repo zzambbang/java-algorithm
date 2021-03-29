81bb16cb5beddc50d811bf7e9578339b3105b01e
zzambbang <zzambbang@gamil.com>
2021-03-29 14:22:34 +0900

    간만에 commit...

--- P210308/src/App.java
+++ P210308/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int sum = 0; 
+        int asum = 0;
+        int bsum = 0;
+
+        for(int i=0; i<=50; i++){
+            if(i%2 == 0){
+                System.out.println("짝수합 :" + (asum += i));
+            }else{
+                System.out.println("홀수합 :" + (bsum += i));
+            }
+            sum = asum + bsum;
+        }
+        System.out.println("전체 누적합 : " + sum);
+    
     }
 }
---
+++ P210308/src/p111_BinSearch.java
@@ -0,0 +1,10 @@
+
+for(i=1; i<51; i++){
+    if(i%2 === 0 ){
+        var asum = 0;
+        console.log('짝수 누적값' + (asum += i));
+    }else{
+        var bsum = 0;
+        console.log('홀수 누적값' + (bsum += i));
+    }
+}console.log('전체 누적값 :' (asum += i)+(bsum += i))
